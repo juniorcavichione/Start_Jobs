47a621a0cae6457bed2f6c4a4f88315de3eb4220
alfabar <alfa_bar@hotmail.com>
2022-01-27 18:24:39 -0300

    atualizado

--- entrar-registrar.php
+++ entrar-registrar.php
@@ -23,78 +23,20 @@
 					</ul>
 				</header>
 
-				<!-- <section id="banner">
-					<div class="content">
-						
-						<h3>Formulario de cadastro</h3>
-						<form method="post" action="#">
-							<div class="row gtr-uniform">
-								<div class="col-6 col-12-xsmall">
-									<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
-								</div>
-								<div class="col-6 col-12-xsmall">
-									<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
-								</div>
-								
-								<div class="col-12">
-									<select name="demo-category" id="demo-category">
-										<option value="">- Category -</option>
-										<option value="1">Manufacturing</option>
-										<option value="1">Shipping</option>
-										<option value="1">Administration</option>
-										<option value="1">Human Resources</option>
-									</select>
-								</div>
-								
-								<div class="col-4 col-12-small">
-									<input type="radio" id="demo-priority-low" name="demo-priority" checked>
-									<label for="demo-priority-low">Low</label>
-								</div>
-								<div class="col-4 col-12-small">
-									<input type="radio" id="demo-priority-normal" name="demo-priority">
-									<label for="demo-priority-normal">Normal</label>
-								</div>
-								<div class="col-4 col-12-small">
-									<input type="radio" id="demo-priority-high" name="demo-priority">
-									<label for="demo-priority-high">High</label>
-								</div>
-								
-								<div class="col-6 col-12-small">
-									<input type="checkbox" id="demo-copy" name="demo-copy">
-									<label for="demo-copy">Email me a copy</label>
-								</div>
-								<div class="col-6 col-12-small">
-									<input type="checkbox" id="demo-human" name="demo-human" checked>
-									<label for="demo-human">I am a human</label>
-								</div>
-								
-								<div class="col-12">
-									<textarea name="demo-message" id="demo-message" placeholder="Enter your message"
-										rows="6"></textarea>
-								</div>
-								
-								<div class="col-12">
-									<ul class="actions">
-										<li><input type="submit" value="Cadastrar" name="cadastrar" class="primary" /></li>
-										<li><input type="reset" value="Reset" /></li>
-									</ul>
-								</div>
-							</div>
-						</form>
-					</div>					
-				</section> -->
+			
 				<!--====================================================
-                    LOGAR OU REGISTRAR
-======================================================-->
+				  LOGAR OU REGISTRAR
+				  ======================================================-->
 				<section id="login">
+					<div class="modal-dialog">
 						<div class="modal-content rounded-5 shadow">
-							<div class="modal-header p-5 pb-4 border-bottom-0">
+							<div class="modal-header pb-4 border-bottom-0">
 								<h2 class="text-center fw-bold mb-0">Entre ou se Cadastre</h2>
 							</div>
 							<div id="div-forms">
 								<form id="login-form">
 									<h3 class="text-center">Login</h3>
-									<div class="modal-body p-5 pt-0">
+									<div class="modal-body pt-0">
 
 										<div class="form-floating mb-3">
 											<input type="email" class="form-control rounded-4" id="email">
@@ -122,7 +64,9 @@
 									</div>
 								</form>
 
-								<!-- RECUPERE SUA SENHA AQUI -->
+								<!--====================================================
+				  RECUPERE A SENHA
+				  ======================================================-->
 
 								<form id="recuperar-form" style="display:none;">
 									<h3 class="text-center">Recupe Sua senha</h3>
@@ -151,7 +95,9 @@
 									</div>
 								</form>
 
-								<!-- REGISTRE SE MODAL  -->
+								<!--====================================================
+				  REGISTRAR
+				  ======================================================-->
 
 								<form id="register-form" style="display:none;">
 									<h3 class="text-center">Registre</h3>
@@ -217,6 +163,7 @@
 							</div>
 
 						</div>
+					</div>
 
 				</section>
 			</div>
