340bbfef348456d29db3f9c0402e6dec5d0d512a
alfabar <alfa_bar@hotmail.com>
2022-01-27 16:04:23 -0300

    atualizado

--- empresa/includes/sidebar.php
+++ empresa/includes/sidebar.php
@@ -12,71 +12,26 @@
         <!-- Menu -->
         <nav id="menu">
             <header class="major">
-                <h2>Menu</h2>
+                <a href="?minha-conta"><h2>Minha conta</h2></a>
+                
             </header>
             <ul>
                 <li><a href="../">Página Inicial</a></li>
-                <li><a href="vagas.php">Vagas</a></li>
+                <li><a href="?minha-vaga">Vagas</a></li>
                 <li>
                     <span class="opener">Vagas por área</span>
                     <ul>
-                        <li><a href="#">Exatas</a></li>
-                        <li><a href="#">Saúde</a></li>
-                        <li><a href="#">Humanas</a></li>
+                        <li><a href="#">Inserir</a></li>
+                        <li><a href="#">Visualizar</a></li>
+                        <li><a href="#">Pretendentes</a></li>
                         <li><a href="#">Serviços Gerais</a></li>
                     </ul>
                 </li>
-                <li><a href="dicas.php">Dicas</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li><a href="noticias.php">Noticias</a></li>
-                <li><a href="contato.php">Fale Conosco</a></li>
+
+                <li><a href="?editar-conta">Editar</a></li>
+                <li><a href="?excluir-conta">Excluir conta</a></li>
             </ul>
         </nav>
-
-        <!-- Section -->
-        <section>
-            <header class="major">
-                <h2>Parceiros</h2>
-            </header>
-            <div class="mini-posts">
-                <article>
-                    <a href="https://www.americanas.com.br/" class="image"><img src="images/americanas.png"
-                            alt="Site de compras Americanas" /></a>
-                    <p>Americanas tem de tudo a toda hora. em qualquer lugar :)</p>
-                </article>
-                <article>
-                    <a href="https://faucetcrypto.com/ref/1251908" class="image"><img src="images/faucet.png"
-                            alt="Ganhe Criptomoedas Grátis" /></a>
-                    <p>Realize pequenas tarefas e Claims de 30 em 30 minutos e ganhe coins e troque por criptomoedas,
-                        pagamos diretamente na sua carteira/wallet.</p>
-                </article>
-                <article>
-                    <a href="https://g1.globo.com/" class="image"><img src="images/G1.jpg"
-                            alt="Maior portal de notícias do Brasil" /></a>
-                    <p>Últimas notícias do Brasil e do mundo, sobre política, economia, emprego, educação, saúde, meio
-                        ambiente, tecnologia, ciência, cultura e carros.</p>
-                </article>
-            </div>
-            <!-- 	<ul class="actions">
-											<li><a href="#" class="button">Mais Parceiros</a></li>
-										</ul> -->
-        </section>
-
-        <!-- Section -->
-        <section>
-            <header class="major">
-                <h2>Fale Conosco</h2>
-            </header>
-            <p>Tem dúvidas sobre os serviços que prestamos, entre em contato conosco.</p>
-            <ul class="contact">
-                <li class="icon solid fa-envelope"><a
-                        href=mailto:penha@sp.senac.br?subject="link HTML">penha@sp.senac.br</a></li>
-                <li class="icon solid fa-phone">(011) 2135-0300</li>
-                <li class="icon solid fa-home">R. Francisco Coimbra, 403 - Penha de França, São Paulo - SP, 03639-000
-                </li>
-            </ul>
-        </section>
-
         <!-- Footer -->
         <footer id="footer">
             <p class="copyright">&copy; Diretos reservado: Alunos Senac Penha - Desenvolvedor Web Full Stack<p>
