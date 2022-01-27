4793ef6a0a76aa2822a68367854089c5aedf85c5
alfabar <alfa_bar@hotmail.com>
2022-01-27 14:13:20 -0300

    atualizado

--- contato.php
+++ contato.php
@@ -1,17 +1,4 @@
-<!DOCTYPE HTML>
-<!--
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-
-<head>
-	<title>Fale Conosco - Start_Jobs</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="assets/css/main.css" />
-</head>
+<?php include_once('./includes/cabecalho.php')?>
 
 <body class="is-preload">
 
@@ -28,15 +15,16 @@
 					<ul class="icons">
 						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a>
-						</li>
+						<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank"
+								class="icon brands fa-github"><span class="label">Github</span></a></li>
 						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
 						<li><a href="login.html" id="login" class="button big">Login</a></li>
+
 					</ul>
 				</header>
 
-				<!-- Content -->
-				<section>
+					<!-- Content -->
+          <section>
 					<header class="main">
 						<h1>Elements</h1>
 					</header>
@@ -508,104 +496,6 @@ print 'It took ' + i + ' iterations to sort the deck.';
 
 			</div>
 		</div>
-
-		<!-- Sidebar -->
-		<div id="sidebar">
-			<div class="inner">
-
-				<!-- Search -->
-				<section id="search" class="alt">
-					<form method="post" action="#">
-						<input type="text" name="query" id="query" placeholder="Search" />
-					</form>
-				</section>
-
-				<!-- Menu -->
-				<nav id="menu">
-					<header class="major">
-						<h2>Menu</h2>
-					</header>
-					<ul>
-						<li><a href="index.php">Página Inicial</a></li>
-						<li><a href="vagas.php">Vagas</a></li>
-						<li>
-							<span class="opener">Vagas por área</span>
-							<ul>
-								<li><a href="#">Exatas</a></li>
-								<li><a href="#">Saúde</a></li>
-								<li><a href="#">Humanas</a></li>
-								<li><a href="#">Serviços Gerais</a></li>
-							</ul>
-						</li>
-						<li><a href="dicas.php">Dicas</a></li>
-						<li><a href="sobre.php">Sobre</a></li>
-						<li><a href="noticias.php">Noticias</a></li>
-						<li><a href="contato.php">Fale Conosco</a></li>
-					</ul>
-				</nav>
-
-				<!-- Section -->
-				<section>
-					<header class="major">
-						<h2>Parceiros</h2>
-					</header>
-					<div class="mini-posts">
-						<article>
-							<a href="https://www.americanas.com.br/" class="image"><img src="images/americanas.png"
-									alt="Site de compras Americanas" /></a>
-							<p>Americanas tem de tudo a toda hora. em qualquer lugar :)</p>
-						</article>
-						<article>
-							<a href="https://faucetcrypto.com/ref/1251908" class="image"><img src="images/faucet.png"
-									alt="Ganhe Criptomoedas Grátis" /></a>
-							<p>Realize pequenas tarefas e Claims de 30 em 30 minutos e ganhe coins e troque por
-								criptomoedas, pagamos diretamente na sua carteira/wallet.</p>
-						</article>
-						<article>
-							<a href="https://g1.globo.com/" class="image"><img src="images/G1.jpg"
-									alt="Maior portal de notícias do Brasil" /></a>
-							<p>Últimas notícias do Brasil e do mundo, sobre política, economia, emprego, educação,
-								saúde, meio ambiente, tecnologia, ciência, cultura e carros.</p>
-						</article>
-					</div>
-					<!-- 		<ul class="actions">
-											<li><a href="#" class="button">Mais Parceiros</a></li>
-										</ul> -->
-				</section>
-
-				<!-- Section -->
-				<section>
-					<header class="major">
-						<h2>Fale Conosco</h2>
-					</header>
-					<p>Tem dúvidas sobre os serviços que prestamos, entre em contato conosco.</p>
-					<ul class="contact">
-						<li class="icon solid fa-envelope"><a
-								href=mailto:penha@sp.senac.br?subject="link HTML">penha@sp.senac.br</a></li>
-						<li class="icon solid fa-phone">(011) 2135-0300</li>
-						<li class="icon solid fa-home">R. Francisco Coimbra, 403 - Penha de França, São Paulo - SP,
-							03639-000
-						</li>
-					</ul>
-				</section>
-
-				<!-- Footer -->
-				<footer id="footer">
-					<p class="copyright">&copy; Diretos reservado: Alunos Senac Penha - Desenvolvedor Web Full Stack<p>
-				</footer>
-
-			</div>
-		</div>
-
+		<?php include_once('./includes/sidebar.php');?>
 	</div>
-
-	<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/browser.min.js"></script>
-	<script src="assets/js/breakpoints.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+	<?php include_once('./includes/footer.php');?>
