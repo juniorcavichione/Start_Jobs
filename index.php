74020f6ab0411c043dcec6d5bc9c8070fcd8aa4e
alfabar <alfa_bar@hotmail.com>
2022-01-27 13:52:04 -0300

    atualizado

--- index.php
+++ index.php
@@ -1,91 +1,60 @@
+<?php ?>
 
-<!DOCTYPE HTML>
-<!--
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		
-		<meta charset="utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		 <!-- Inserindo tags metas -->
-  <!-- Descição não ultrapassar 160 caracteres -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Encontre seu emprego aqui de forma totalmente gratuita">
-  <!-- Palavras chaves Quanto menos palavras chaves melhor-->
-  <meta name="keywords" content="Emprego, Trabalho">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<title>Start_Jobs</title>
-		<!-- Inserindo o Favicon -->
-		<link rel="shortcut icon" href="images/favicon.jpg" type="image/x-icon">
-	</head>
-	<body class="is-preload">
+<body class="is-preload">
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+	<!-- Wrapper -->
+	<div id="wrapper">
 
-				<!-- Main -->
-					<div id="main">
-						<div class="inner">
+		<!-- Main -->
+		<div id="main">
+			<div class="inner">
 
-						
+				<!-- Header -->
+				<header id="header">
+					<a href="index.php" class="logo"><strong>Start_<span style="color: #2c76ee">Jobs</span></strong></a>
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank"
+								class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="login.html" id="login" class="button big">Login</a></li>
 
-							<!-- Header -->
-								<header id="header">
-									<a href="index.php" class="logo"><strong>Start_<span style="color: #2c76ee">Jobs</span></strong></a>
-									<ul class="icons">
-										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-										<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank" class="icon brands fa-github"><span class="label">Github</span></a></li>
-										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="login.html" id="login" class="button big">Login</a></li>
-										
-																			
-									</ul>
-								</header>		
-								
+					</ul>
+				</header>
 
-
-							<!-- Banner -->
-								<section id="banner">
-									<div class="content">
-										<header>
-											<h1>Start Jobs<br />
-											Sua busca pelo emprego, num clique!</h1>
-											<p>Site responsível</p>
-										</header>
-										<p>Vamos te ajudar na busca do emprego desejado, temos vagas em diversos segmentos.</p>
-										<p>Temos vagas de:</p>
-										<ul>
-											<li>Informática | TI</li>
-											<li>Logística</li>
-											<li>Administração</li>
-											<li>Serviços</li>
-											<li>Free Lancer</li>
-										</ul>
-										<ul class="actions">
-											<li><a href="vagas.php" class="button big">Página de vagas</a></li>
-										</ul>
-									</div>
-									<span class="image object">
-										<!-- <img src="images/pic10.jpg" alt="" /> -->
-									</span>
-								</section>
-
-
-								<hr>
-								<!-- VAGAS  BACK-END -->
-<h2>Vagas</h2>
-
-							
-						</div>
+				<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+							<h1>Start Jobs<br />
+								Sua busca pelo emprego, num clique!</h1>
+							<p>Site responsível</p>
+						</header>
+						<p>Vamos te ajudar na busca do emprego desejado, temos vagas em diversos segmentos.</p>
+						<p>Temos vagas de:</p>
+						<ul>
+							<li>Informática | TI</li>
+							<li>Logística</li>
+							<li>Administração</li>
+							<li>Serviços</li>
+							<li>Free Lancer</li>
+						</ul>
+						<ul class="actions">
+							<li><a href="vagas.php" class="button big">Página de vagas</a></li>
+						</ul>
 					</div>
+					<span class="image object">
+						<!-- <img src="images/pic10.jpg" alt="" /> -->
+					</span>
+				</section>
 
-				<?php include_once('./includes/sidebar.php');?>
+				<hr>
+				<!-- VAGAS  BACK-END -->
+				<h2>Vagas</h2>
 
 			</div>
-			<?php include_once('./includes/footer.php');?>
-
+		</div>
+		<?php include_once('./includes/sidebar.php');?>
+	</div>
+	<?php include_once('./includes/footer.php');?>
