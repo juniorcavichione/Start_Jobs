61ab03f22abfa1bab878a1021776de0f6968d970
alfabar <alfa_bar@hotmail.com>
2022-01-27 16:24:45 -0300

    atualizado

--- empresa/index.php
+++ empresa/index.php
@@ -1,14 +1,11 @@
 <?php include_once('./includes/cabecalho.php')?>
 
 <body class="is-preload">
-
 	<!-- Wrapper -->
 	<div id="wrapper">
-
 		<!-- Main -->
 		<div id="main">
 			<div class="inner">
-
 				<!-- Header -->
 				<header id="header">
 					<a href="index.php" class="logo"><strong>Start_<span style="color: #2c76ee">Jobs</span></strong></a>
@@ -19,10 +16,10 @@
 								class="icon brands fa-github"><span class="label">Github</span></a></li>
 						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
 						<li><a href="login.html" id="login" class="button big">Login</a></li>
-
 					</ul>
 				</header>
 
+				<!-- NAVEGAÇÃO POR GET  -->
 				<?php 
 				if(isset($_GET['minha-conta'])){
 					include("minha-conta.php");
@@ -32,10 +29,8 @@
 					include("minha-vaga.php");
 				} if(isset($_GET['excluir-conta'])){
 					include("excluir-conta.php");
-				}
-				
-				?>
-			
+				}				
+				?>			
 			</div>
 		</div>
 		<?php include_once('./includes/sidebar.php');?>
