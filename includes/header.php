3c592424d6ea5637525085f0f7122e8a01a79a7a
Tiago Ferreira <52896114+tiagoferreira18@users.noreply.github.com>
2022-01-28 10:02:18 -0300

    include do header

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!-- Header -->
+<header id="header">
+					<a href="index.php" class="logo"><strong>Start_<span style="color: #2c76ee">Jobs</span></strong></a>
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank"
+								class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="entrar-registrar.php" id="login" class="button big">Entrar / Registrar</a></li>
+
+					</ul>
+				</header>
