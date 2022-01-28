7f23056d8fd29a36a6baa05fc1ceb2f9a38cdd93
alfabar <alfa_bar@hotmail.com>
2022-01-28 10:49:41 -0300

    atualizado

--- contato.php
+++ contato.php
@@ -11,10 +11,10 @@ require './phpMailer/SMTP.php';
 
 if(isset($_POST['enviar'])){
 
-	$nomeCompleto = $_POST['inputName'];
-	$emailUser = $_POST['inputEmail'];
-	$contatoNumero = $_POST['inputContato'];
-	$assuntoUser = $_POST['AssuntoAqui'];
+	$nomeCompleto = $_POST['nome'];
+	$emailUser = $_POST['email'];
+	$contatoNumero = $_POST['contato'];
+	$assuntoUser = $_POST['mensagem'];
   
   
 	$mail = new PHPMailer(true);
@@ -81,28 +81,23 @@ if(isset($_POST['enviar'])){
 					<form method="post" action="#">
 								<div class="row gtr-uniform">
 									<div class="col-6 col-12-xsmall">
-										<input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+										<input type="text" name="nome" id="demo-name" value="" />
 									</div>
 									<div class="col-6 col-12-xsmall">
-										<input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+										<input type="email" name="email" id="demo-email" value="" placeholder="Email" />
 									</div>								
 									<!-- Break -->
-									<div class="col-6 col-12-small">
-										<input type="checkbox" id="demo-copy" name="demo-copy">
-										<label for="demo-copy">Email me a copy</label>
-									</div>
-									<div class="col-6 col-12-small">
-										<input type="checkbox" id="demo-human" name="demo-human" checked>
-										<label for="demo-human">I am a human</label>
+									<div class="col-12">
+										<input type="text" name="contato" id="" value="" placeholder="Telefone" />
 									</div>
 									<!-- Break -->
 									<div class="col-12">
-										<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+										<textarea name="mensagem" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
 									</div>
 									<!-- Break -->
 									<div class="col-12">
 										<ul class="actions">
-											<li><input type="submit" value="Send Message" class="primary" /></li>
+											<li><input type="submit" value="Send Message" name="enviar" class="primary" /></li>
 											<li><input type="reset" value="Reset" /></li>
 										</ul>
 									</div>
