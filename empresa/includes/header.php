77822c63881397511dc36235dcc6857cc31a6654
alfabar <alfa_bar@hotmail.com>
2022-01-30 09:56:38 -0300

    atualizado

--- empresa/includes/header.php
+++ empresa/includes/header.php
@@ -4,14 +4,8 @@
 	<ul class="icons">
 		<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
 		<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-		<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank" class="icon brands fa-github"><span
-					class="label">Github</span></a></li>
+		<li><a href="https://github.com/juniorcavichione/Start_Jobs" target="_blank" class="icon brands fa-github"><span class="label">Github</span></a></li>
 		<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-		<li><?php if($_SESSION['tipo'] != ""){
-			echo "<a href='?sair' id='login' class='button big'>Sair</a>";
-			}else{
-				echo "<a href='entrar-registrar.php' id='login' class='button big'>Sair / Registrar</a>";
-			}?></li>
-
+		<li><a href="?sair" id="login" class="button big">Sair</a></li>
 	</ul>
 </header>
