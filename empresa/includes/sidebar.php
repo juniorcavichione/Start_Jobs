8426d62a5ce58c63a79881cc49a36847355f5cc7
alfabar <alfa_bar@hotmail.com>
2022-01-31 19:12:39 -0300

    atualizado

--- empresa/includes/sidebar.php
+++ empresa/includes/sidebar.php
@@ -1,19 +1,16 @@
 <!-- Sidebar -->
 <div id="sidebar">
     <div class="inner">
-
         <!-- Search -->
         <section id="search" class="alt">
             <form method="post" action="#">
                 <input type="text" name="query" id="query" placeholder="Procure Aqui" />
             </form>
         </section>
-
         <!-- Menu -->
         <nav id="menu">
             <header class="major">
-                <a href="?minha-conta"><h2>Minha conta</h2></a>
-                
+                <a href="?minha-conta"><h2>Minha conta</h2></a>                
             </header>
             <ul>
                 <li><a href="../">Página Inicial</a></li>
@@ -23,19 +20,17 @@
                     <ul>
                         <li><a href="index.php?inserir-vaga">Inserir</a></li>
                         <li><a href="#">Visualizar</a></li>
-                        <li><a href="#">Pretendentes</a></li>
+                        <li><a href="index.php?listar-pretedente">Pretendentes</a></li>
                         <li><a href="#">Serviços Gerais</a></li>
                     </ul>
                 </li>
-
-                <li><a href="?editar-conta">Editar</a></li>
-                <li><a href="?excluir-conta">Excluir conta</a></li>
+                <li><a href="index.php?editar-conta">Editar</a></li>
+                <li><a href="index.php?excluir-conta">Excluir conta</a></li>
             </ul>
         </nav>
         <!-- Footer -->
         <footer id="footer">
             <p class="copyright">&copy; Diretos reservado: Alunos Senac Penha - Desenvolvedor Web Full Stack<p>
         </footer>
-
     </div>
 </div>
