624ff96e0579be10d46f7eb1e7b8071b12348f25
alfabar <alfa_bar@hotmail.com>
2022-01-31 22:44:05 -0300

    atualizado

---
+++ empresa/detalhe-candidato.php
@@ -0,0 +1,53 @@
+<?php require_once "../src/Vaga.php";
+
+?>	
+	<!-- Banner -->
+	<section id="banner" class="container">
+	    <div class="content">
+	        <header>
+	            <h1>Contrata Candidato</h1>
+	            <p>Site responsível</p>
+	        </header>		
+			<div class="container py-3">
+			<div class="row mb-2">
+				<div class="col-md-6">
+				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+					<div class="col p-4 d-flex flex-column position-static">
+					<strong class="d-inline-block mb-2 text-primary">Nome Completo</strong>
+					<h3 class="mb-0">Featured post</h3>
+					<div class="mb-1 text-muted">Nov 12</div>
+					<ul>
+						<li>Informática | TI</li>
+						<li>Logística</li>
+						<li>Administração</li>
+						<li>Serviços</li>
+						<li>Free Lancer</li>
+					</ul>
+					<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="stretched-link">Continue reading</a>
+					</div>
+					<div class="col-auto d-none d-lg-block">
+					<svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+					</div>
+				</div>
+				</div>
+				<div class="col-md-6">
+				<div class="row g-0 rounded overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
+					<div class="col p-4 d-flex flex-column position-static">
+					<strong class="d-inline-block mb-2 text-success">Descrição</strong>
+					<h3 class="mb-0">Região</h3>
+					<div class="mb-1 text-muted">Data candidatura</div>
+					<p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+					<a href="index.php?contratar-candidato" class="button big">Contratar</a>
+					</div>
+					<div class="col-auto d-none d-lg-block">
+					<!-- <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+
+					</div>
+				</div>
+				</div>
+			</div>
+			</div>
+	    </div>
+	</section>
