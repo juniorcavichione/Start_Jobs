71046580b2bd8c29443493209696c74543859738
Alicio Cavichione Junior <juhyoga@yahoo.com.br>
2022-02-01 11:47:18 -0300

    atualização da index, eu estou atualizando, ok chefe?

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once('./includes/cabecalho.php')?>
+<?php include_once('./includes/cabecalho.php') ?>
 
 <body class="is-preload">
 	<!-- Wrapper -->
@@ -6,44 +6,15 @@
 		<!-- Main -->
 		<div id="main">
 			<div class="inner">
-				<?php include "includes/header.php"?>
+				<?php include "includes/header.php" ?>
 				<!-- Banner -->
 				<section id="banner">
 					<div class="content">
-						<header class="py-5 border-bottom">
-							<div class="container pt-md-1 pb-md-4">
-								<div class="row">
-									<div class="col-xl-8">
-										<h1 class="bd-title mt-0">Examples</h1>
-										<p class="bd-lead">Quickly get a project started with any of our examples
-											ranging from using parts of the framework to custom components and layouts.
-										</p>
+						<h3> Nossa plataforma foi desenvolvida pensando em no candidato e na empresa.</h3>
+						<h3>Candidatos que estão procurando o seu primeiro emprego ou realocação.</h3>
+						<h3>Empresa que está em plena expansão e quer oferecer vagas em diferentes plataformas.</h3>
 
-										<div class="d-flex flex-column flex-sm-row">
-											<a href="https://github.com/twbs/bootstrap/releases/download/v5.0.2/bootstrap-5.0.2-examples.zip"
-												class="btn btn-lg btn-bd-primary"
-												onclick="ga('send', 'event', 'Examples', 'Hero', 'Download Examples');">Download
-												examples</a>
-											<a href="https://github.com/twbs/bootstrap/archive/v5.0.2.zip"
-												class="btn btn-lg btn-outline-secondary mt-3 mt-sm-0 ms-sm-3"
-												onclick="ga('send', 'event', 'Examples', 'Hero', 'Download');">Download
-												source code</a>
-										</div>
-
-									</div>
-									<div class="col-xl-4 d-lg-flex justify-content-xl-end">
-										<script async=""
-											src="https://cdn.carbonads.com/carbon.js?serve=CKYIKKJL&amp;placement=getbootstrapcom"
-											id="_carbonads_js"></script>
-
-									</div>
-								</div>
-							</div>
-						</header>
-
-						
-						<p>Vamos te ajudar na busca do emprego desejado, temos vagas em diversos segmentos.</p>
-						<p>Temos vagas de:</p>
+						<p><b>Vamos te ajudar na busca do emprego desejado, temos vagas em diversos segmentos.</b></p>
 						<ul>
 							<li>Informática | TI</li>
 							<li>Logística</li>
@@ -58,12 +29,72 @@
 					<span class="image object">
 						<!-- <img src="images/pic10.jpg" alt="" /> -->
 					</span>
+					<hr>
+					<h2>Cartão de apresentação</h2>
+					<h3>O que é uma carta de motivação e para que serve?</h3>
+					<p>Uma carta de motivação é um texto de uma página que explica de forma resumida por que você é o candidato ideal para determinada vaga. Normalmente, o documento é um tipo de introdução que acompanha o currículo ou, no caso de bolsas de estudo e cursos internacionais, a ficha de inscrição.
+
+						É importante não confundir a carta de motivação com a carta de apresentação, que apenas destaca informações específicas do currículo que correspondem aos requisitos da vaga a que você quer se candidatar.
+
+						A carta motivacional vai além da carta de apresentação porque apresenta argumentos para ser escolhido pela empresa ou instituição. A ideia, então, é que você demonstre esse entusiasmo no texto.</p>
+					<h3>Como criar uma carta de motivação?</h3>
+					<p>A carta de motivação deve ter uma página, no máximo. No texto, você deve se apresentar, resumir seu histórico e dizer por que tem interesse na vaga ou na organização a que está se candidatando. </p>
+					<h3>Estrutura da carta de apresentação</h3>
+					<p>Sua carta deve ter pelo menos três parágrafos (escreva mais se você tiver mais informações para incluir). A ideia é que ela seja composta por três partes principais: </p>
+					<ul>
+					<li>Introdução: é onde você diz quem é, o que faz e por que está se candidatando. </li>
+					<li>Parte principal: nesta parte você “vende seu peixe”, contando suas realizações, falando sobre suas competências e habilidades e explicando seu entusiasmo com a oportunidade. Sempre que possível, use dados que comprovem o que você está dizendo.</li>
+					<li>Conclusão: nesta parte final você diz de que forma acredita que pode contribuir com a empresa ou área de estudo e termina apontando seu objetivo principal (fazer parte da equipe, por exemplo). Depois, basta agradecer a atenção de quem estiver lendo o texto. </li>
+					</ul>
+					<h3>Exemplos de carta de motivação</h3>
+					<p>Exemplo 1: Carta de motivação de profissional com experiência
+
+						Caro (nome da pessoa que vai ler a sua carta, se você tiver essa informação),
+
+						Meu nome é Antonio Luiz e gostaria de expressar meu interesse pela vaga de coordenador de telemarketing da EmpresaX.
+
+						Tenho xx anos de experiência no segmento de telemarketing e conheço bem os processos e as atividades da função (complete com os requisitos da vaga que você cumpre).
+
+						Acredito que minha experiência seja interessante para a empresa porque já alcancei resultados relevantes, como redução de XX% no tempo de resposta aos clientes e aumento de XX% no nível de satisfação com o atendimento (cite exemplos numéricos, sempre que tiver). Eu gosto de desafios e por isso me sinto bastante entusiasmado com a oportunidade de trabalhar com vocês.
+
+						Por fim, quero dizer que tenho muita vontade de trabalhar na EmpresaX inclusive porque acompanho suas ações sociais e admiro seu comprometimento com a comunidade local do XXXXX. Eu mesmo faço trabalho voluntário na região há XX anos e entendo o quanto é importante levar cultura e entretenimento para os moradores do bairro.
+
+						Muito obrigado pela atenção,
+						Antonio Luiz. </p>
+
+					<p>
+						Exemplo 2: Carta de motivação de profissional sem experiência
+
+						Caro (nome da pessoa que vai ler a sua carta, se você tiver essa informação. Se não tiver, use apenas “Caros”),
+
+						Meu nome é Antonia Luiza e escrevo para demonstrar meu interesse pela vaga de analista de marketing Jr da EmpresaY.
+
+						Eu me formei em Marketing em julho de 2020, tenho inglês intermediário e disponibilidade para trabalho integral na região central da cidade (liste os requisitos da vaga que você cumpre).
+
+						Desde o primeiro ano da faculdade fiz parte da Empresa Jr e participei de xx projetos na área de marketing que trouxeram resultados interessantes para os clientes. Um deles, por exemplo, conseguiu aumentar vendas em 20% durante a pandemia com a nossa sugestão de investir em conteúdo nas redes sociais e atendimento por Whatsapp.
+
+						Acredito que minha experiência em desenvolver projetos do começo ao fim, sob pressão de prazos e trabalhando em equipe, possa ser útil aos novos projetos da empresa X voltados a inovação e novas tecnologias.
+
+						Gostaria ainda de dizer que acompanho as notícias da EmpresaY e me identifico muito com sua forma de trabalho. Adoraria trabalhar em uma empresa em que todas as ideias são bem-vindas e todos são responsáveis pelas suas entregas.
+
+						Muito obrigada pela atenção,
+
+						Antonia Luiza. </p>
+
+					<p>Exemplo 3: carta de motivação para universidade
+
+						Caro (nome da pessoa que vai ler a sua carta, se você tiver essa informação. Se não tiver, use apenas “Caros”),
+
+						Eu sou formado em Administração e descobri minha paixão pelo Marketing Online durante um estágio na EmpresaZ. O trabalho me deu uma visão ampla, mas superficial, da maioria dos canais de marketing digital. Agora, gostaria de aprofundar meu conhecimento sobre o tema participando do programa de Marketing Digital da UniversidadeAB.
+
+						Muito obrigada pela atenção,
+						Francisco Gama. </p>
 				</section>
 				<hr>
 				<!-- VAGAS  BACK-END -->
 				<h2>Vagas</h2>
 			</div>
 		</div>
-		<?php include_once('./includes/sidebar.php');?>
+		<?php include_once('./includes/sidebar.php'); ?>
 	</div>
-	<?php include_once('./includes/footer.php');?>
+	<?php include_once('./includes/footer.php'); ?>
