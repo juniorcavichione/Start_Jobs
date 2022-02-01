a70880d9e15437cc56d615254317cdc646fc8a3b
Tiago Ferreira <52896114+tiagoferreira18@users.noreply.github.com>
2022-02-01 09:29:55 -0300

    vagas formulario de pesquisa

--- vagas.php
+++ vagas.php
@@ -9,7 +9,23 @@
 				<!-- Content -->
 				<section>
 					<header class="main">
-						<h1>Pesquise aqui a vaga desejada</h1>
+						
+						<!-- Botão de pesquisar vagas -->
+						<div class="jumbotron my-4 shadow text-center">
+            <h1 class="display-4">ENCONTRE SUA VAGA AQUI!</h1>
+
+            <!-- Campo de pesquisa -->
+				 <p class="lead">Consultar</p>
+
+            <!-- autocomplete off desliga o historico -->
+            <form autocomplete="off" action="resultados.php" method="get" class="form-inline justify-content-center">
+				
+                <input placeholder="Pesquise aqui" type="search" name="busca" id="busca" class="form-control mr-2">
+
+                <button class="btn btn-primary" type="submit" >OK</button>
+            </form>
+
+
 					</header>
 				</section>
 			</div>
